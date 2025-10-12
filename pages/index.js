626a708a3abcad1e12f144a1548b8b9a85dbf904
neoeduardo <neo.eduardoaraujo@gmail.com>
2025-10-12 22:07:02 +0000

    adiciona e configura o prettier e o editorconfig

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <p>Garçom, um sushi com goiabada por favor. Pra sobremesa um cookie de Red Velvet recheado com brigadeiro de cream chesse. A conta fica por conta da Barbara Sofia</p>
+function Home() {
+  return (
+    <p>
+      Garçom, um sushi com goiabada por favor. Pra sobremesa um cookie de Red
+      Velvet recheado com brigadeiro de cream chesse. A conta fica por conta da
+      Barbara Sofia
+    </p>
+  );
 }
 
 export default Home;
