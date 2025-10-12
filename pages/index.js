2a440396b23a8f81abcf4ba582b2fa8d26c76be1
Eduardo Araujo <neo.eduardoaraujo@gmail.com>
2025-10-12 03:01:38 -0300

    Alterando novamente o texto da página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Título do Webapp</h1>
+    return <p>Garçom, um sushi com goiabada por favor. Pra sobremesa um cookie de Red Velvet recheado com brigadeiro de cream chesse. A conta fica por conta da Barbara Sofia</p>
 }
 
 export default Home;
