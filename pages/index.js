2e4559fac4823e88fef1a3360343f0631ecf1362
neoeduardo <neo.eduardoaraujo@gmail.com>
2025-10-14 00:04:06 +0000

    atualizando para novo texto

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ function Home() {
     <>
       <h1>Barbara Sofia</h1>
       <h2>Cadê meu Cookie Red Velvet com brigadeira de Cream Cheese?</h2>
+      <p>
+        Sem pressa:
+        <a href="https://youtu.be/DRn2ZawV-4w?si=rtu5YcMGBA6OI95a">🍪</a>
+      </p>
     </>
   );
 }
