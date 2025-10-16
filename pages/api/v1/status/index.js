2521ef4b15a3e843da345afd1af5c72c28eb731f
neoeduardo <neo.eduardoaraujo@gmail.com>
2025-10-16 21:43:36 +0000

    montando corpo de resposta para endpoint /api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,17 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
-  response.status(200).json({ mensagem: "sushi é melhor que cuscuz" });
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SHOW server_version;");
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion.rows[0].server_version,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
   expect(response.status).toBe(200);
+
+  // updated_at foi definido?
+  expect(responseBody.updated_at).toBeDefined();
+
+  // conteúdo de updated_at é de fato uma ISOString?
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parseUpdatedAt).toEqual(responseBody.updated_at);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
 });
