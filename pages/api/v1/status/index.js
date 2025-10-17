74c4ef4acee971614af2f9c5e26ef3895f523aa2
neoeduardo <neo.eduardoaraujo@gmail.com>
2025-10-17 21:44:01 +0000

    finaliza o endpoint /api/v1/status com sanitização de query

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,12 +3,21 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
   const postgresVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: postgresVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: openedConnections.rows[0].count,
       },
     },
   });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,12 +3,13 @@ test("GET /api/v1/status should return 200", async () => {
   const responseBody = await response.json();
   expect(response.status).toBe(200);
 
-  // updated_at foi definido?
-  expect(responseBody.updated_at).toBeDefined();
-
   // conteúdo de updated_at é de fato uma ISOString?
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(parseUpdatedAt).toEqual(responseBody.updated_at);
 
   expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
