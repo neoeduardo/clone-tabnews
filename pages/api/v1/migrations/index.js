062d44ff70e6019f37a0fcf80c314a61b8acad95
neoeduardo <neo.eduardoaraujo@gmail.com>
2025-10-22 20:41:37 +0000

    tentativa de corrigir o bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,7 +2,7 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  const defaultMigrationOptions = migrationRunner({
+  const defaultMigrationOptions = await migrationRunner({
     databaseUrl: process.env.DATABASE_URL,
     dryRun: true,
     dir: join("infra", "migrations"), // Evita conflito de DIR entre diferentes SO
